4a7b19069ae84f12f5bdf0bf877dd3c32e325e5d
minalan79 <minalan79@gmail.com>
2024-12-23 12:38:19 +0530

    Sequelize ORM test

--- index.js
+++ index.js
@@ -1,25 +1,84 @@
-import pg from "pg";
 import dotenv from "dotenv";
+import express from "express";
+import { Sequelize } from "sequelize";
+
+const app = express();
 
 dotenv.config();
+app.use(express.json());
 
-const { Client } = pg;
+// const { Client } = pg;
 
-const postgreClient = new Client({
-  user: process.env.POSTGRE_USER,
-  password: process.env.POSTGRE_PASSWORD,
-  host: process.env.POSTGRE_HOST,
-  port: process.env.POSTGRE_PORT,
-  database: process.env.POSTGRE_DBNAME,
-  ssl: {
-    require: true,
-    rejectUnauthorized: false,
-  },
+// const postgreClient = new Client({
+//   user: process.env.POSTGRE_USER,
+//   password: process.env.POSTGRE_PASSWORD,
+//   host: process.env.POSTGRE_HOST,
+//   port: process.env.POSTGRE_PORT,
+//   database: process.env.POSTGRE_DBNAME,
+//   ssl: {
+//     require: true,
+//     rejectUnauthorized: false,
+//   },
+// });
+
+const sequelize = new Sequelize(
+  process.env.POSTGRE_DBNAME,
+  process.env.POSTGRE_USER,
+  process.env.POSTGRE_PASSWORD,
+  {
+    host: process.env.POSTGRE_HOST,
+    port: process.env.POSTGRE_PORT,
+    dialect: "postgres",
+  }
+);
+
+try {
+  await sequelize.authenticate();
+  console.log("Connection has been established successfully.");
+} catch (error) {
+  console.error("Unable to connect to the database:", error);
+}
+// postgreClient.connect();
+
+function generateShortURL(longURL) {}
+
+// const result = await postgreClient.query("SELECT * FROM pg_database");
+// console.log(result);
+
+// await postgreClient.end();
+
+app.post("/shorten", (req, res) => {
+  try {
+    const data = req.body;
+    res.status(201).json();
+  } catch {}
 });
 
-postgreClient.connect();
+app.get("/shorten/:shortURL", (req, res) => {
+  try {
+    const shortURL = req.params.shortURL;
+    res.status(200).json();
+  } catch {}
+});
 
-const result = await postgreClient.query("SELECT NOW()");
-console.log(result);
+app.put("/shorten/:shortURL", (req, res) => {
+  try {
+    const shortURL = req.params.shortURL;
+    const data = req.body;
+    res.status(200).json();
+  } catch {}
+});
 
-await postgreClient.end();
+app.delete("/shorten/:shortURL", (req, res) => {
+  try {
+    const shortURL = req.params.shortURL;
+    res.status(204);
+  } catch {}
+});
+
+app.get("/shorten/:shortURL/stats", (req, res) => {
+  try {
+    const shortURL = req.params.shortURL;
+    res.status(200).json();
+  } catch {}
+});
